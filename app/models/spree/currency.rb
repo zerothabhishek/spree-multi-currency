11da19348de83f577b7fb35ba3f6b80011a85e27
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2013-01-07 14:09:54 -0600

    Fixing bug in convert_to function

--- app/models/spree/currency.rb
+++ app/models/spree/currency.rb
@@ -54,7 +54,7 @@ module Spree
 
       def convert_to(value, to, options={})
         basic
-        to_currency = Spree::Currency.find_by_char_code(to) 
+        to_currency = Spree::Currency.find_by_char_code(to.upcase) 
         return value if @to_currency.char_code == @basic.char_code
         if @rate = to_currency.currency_converters.get_rate(options[:date] || Time.now)
           add_rate(@basic.char_code,   to_currency.char_code, @rate.nominal/@rate.value.to_f)
