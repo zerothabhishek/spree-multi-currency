9443f4409018d0c260c9fe34dd4bc2726d849823
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2013-01-07 14:03:37 -0600

    Adding validation when currency to convert is basic

--- app/models/spree/currency.rb
+++ app/models/spree/currency.rb
@@ -55,6 +55,7 @@ module Spree
       def convert_to(value, to, options={})
         basic
         to_currency = Spree::Currency.find_by_char_code(to) 
+        return value if @to_currency.char_code == @basic.char_code
         if @rate = to_currency.currency_converters.get_rate(options[:date] || Time.now)
           add_rate(@basic.char_code,   to_currency.char_code, @rate.nominal/@rate.value.to_f)
           add_rate(to_currency.char_code, @basic.char_code,   @rate.value.to_f)
