efb5cceb24437f259728809e84f2f7d28d4ad259
Tobias Bohwalli <hi@futhr.io>
2013-06-27 17:07:06 +0200

    Add testing support for feature tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-if ENV["COVERAGE"]
+if ENV['COVERAGE']
   require 'simplecov'
   require 'coveralls'
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
@@ -10,7 +10,6 @@ if ENV["COVERAGE"]
     add_group 'Controllers', 'app/controllers'
     add_group 'Overrides', 'app/overrides'
     add_group 'Models', 'app/models'
-    add_group 'Views', 'app/views'
     add_group 'Libraries', 'lib'
   end
 end
@@ -20,11 +19,42 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/webkit'
+require 'ffaker'
+require 'database_cleaner'
 
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/url_helpers'
+
 RSpec.configure do |config|
+  config.include Capybara::DSL, type: :request
+  config.include Spree::TestingSupport::ControllerRequests
+  config.include FactoryGirl::Syntax::Methods
+  config.include Spree::TestingSupport::UrlHelpers
+
+  config.extend Spree::TestingSupport::AuthorizationHelpers::Request, type: :feature
+
   config.mock_with :rspec
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.fail_fast = ENV['FAIL_FAST'] || false
+
+  config.before do
+    if example.metadata[:js]
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+
+  Capybara.javascript_driver = :webkit
 end
